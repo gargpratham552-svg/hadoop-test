4fa7cf174bbd01fc0be9503b910856ca15443c22
pratham <gargpratham552@gmail.com>
2026-04-17 12:38:47 +0530

    Updated UI in script.js

--- public/script.js
+++ public/script.js
@@ -1,45 +1,15 @@
-let shuffled = shuffle(questions);
-
 window.onload = () => {
   shuffled.forEach((q,i)=>{
-    let html = `<p>${i+1}. ${q.q}</p>`;
+    let html = `<div class="question">
+      <p><b>${i+1}. ${q.q}</b></p>`;
+      
     q.options.forEach((opt,j)=>{
-      html += `<input type="radio" name="q${i}" value="${j}">${opt}<br>`;
+      html += `<label>
+        <input type="radio" name="q${i}" value="${j}"> ${opt}
+      </label><br>`;
     });
+
+    html += `</div>`;
     quiz.innerHTML += html;
   });
 };
-
-// tab switch detect
-document.addEventListener("visibilitychange", ()=>{
-  if(document.hidden){
-    alert("Tab switched! Auto submit.");
-    submitTest();
-  }
-});
-
-function submitTest(){
-  let score = 0;
-
-  shuffled.forEach((q,i)=>{
-    let ans = document.querySelector(`input[name=q${i}]:checked`);
-    if(ans && parseInt(ans.value) === q.answer){
-      score++;
-    }
-  });
-
-  let student = JSON.parse(localStorage.getItem("student"));
-
-  fetch("/submit", {
-    method:"POST",
-    headers:{"Content-Type":"application/json"},
-    body: JSON.stringify({
-      ...student,
-      score: score,
-      total: shuffled.length
-    })
-  });
-
-  alert("Score: " + score);
-  window.location = "/";
-}
